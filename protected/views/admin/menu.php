5c8321fb85e84598ced103064f59ea620a8f8058
Alonso Caballero <g.alonso.ct@gmail.com>
2013-10-11 16:54:43 -0500

    cambios backend 50%

--- protected/views/admin/menu.php
+++ protected/views/admin/menu.php
@@ -2,26 +2,41 @@
 <!-- Navigation bar starts -->
           <div class="navbar-back">
                 <ul class="nav-back">
-                   <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin">Inicio</a></li>
+
+                  <li class="group">
+                    <h3>Sesión</h3>
+                    <ul>
+                   <li class="home"><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin">Inicio</a></li>
+                   <li class="salir"><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/logout">Salir</a></li>
+                 </ul>
+               </li>
                    
-                   <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administrar Productos<b class="caret"></b></a>
-                      <ul class="dropdown-menu">
+                   
+                   <li class="group">
+                      <h3>Administrar Productos</h3>
+                      <ul>
                         <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/productos/create">Nuevo Producto</a></li>
                         <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/productos/admin">Consultar Productos</a></li>
                       </ul>
                    </li>                   
-                   <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administrar Líneas<b class="caret"></b></a>
-                      <ul class="dropdown-menu">
+                   <li  class="group">
+                      <h3>Administrar Líneas</h3>
+                      <ul>
                         <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/lineas/create">Nueva Línea (Categoría)</a></li>
                         <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/lineas/admin">Consultar Lineas</a></li>
                       </ul>
                    </li>
+
+                   <li class="group">
+                    <h3>Administrar Secciones</h3>
+                    <ul>
                    <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin/inicio">Adm. Inicio</a></li>
                    <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin/nosotros">Adm. Nosotros</a></li>
-                   <li><a href="#">Adm. Slider</a></li>
-                   <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/logout">Salir</a></li>
+                   <li><a href="#">Adm. Slider</a>
+                    </ul>
+                    </li>
                  </ul>
+
+
          </div>
 
